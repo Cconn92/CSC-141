cfb088198be789430c1903dd35c28aea60b9536b
Cconn92 <c.connors001@albright.edu>
2025-09-03 09:25:47 -0400

    Add files via upload

---
+++ assighnment2 work.py
@@ -0,0 +1,52 @@
+#chris connors 2-1
+message = "hello, I am Chris"
+print(message)
+#chris Connors 2-2
+message = "hello, i am also Chris"
+print(message)
+# 2-3
+first_name = "chris"
+Last_name = "Connors"
+Full_name = f"{first_name} {Last_name}"
+message = (f"good morning, {Full_name.title()}")
+print(message)
+#2-4
+print(Full_name.lower())
+print(Full_name.upper())
+print(Full_name.title())
+#2-5
+message = "Micheal Jordan once said \"I've never lost a game, I just ran out of time.\""
+print(message)
+#2-6
+first_name = "Micheal"
+Last_name = "Jordan"
+famous_person = f"{first_name} {Last_name}"
+#micheal jordan is my faovrite basketball player
+message = (f"{famous_person.title()} once said \"I've never lost a game, I just ran out of time.\"")
+print(message)
+#2-7 (recieved an example for the Whitespace Stripping)
+first_name2 = "chris"
+Last_name2 = "Connors"
+Full_name2 = f"{first_name2} {Last_name2}"
+rstrip = ("\t" + Full_name2.rstrip())
+lstrip = ("\t" + Full_name2.lstrip())
+strip = ("\t" + Full_name2.strip())
+print(Full_name2)
+print("Whitespace Stripping:\n\t" + rstrip + lstrip + strip)
+#2-8(recieved example)
+filename = 'python_notes.txt'
+cleaned_filename = filename.removesuffix('.txt')
+print(cleaned_filename)
+#2-9 
+print(5+3)
+print(4*2)
+print(8/1)
+print(16-8)
+#this program is very helpful for math
+#2-10
+favorite_number = "7"
+message = f"my favorite number is {favorite_number}"
+print(message)
+#2-11
+#commented above (line 24 & 45)
+#2-12 skimmed through
