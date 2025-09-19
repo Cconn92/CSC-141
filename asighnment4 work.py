58444d59dda9f0fed876898c969a4df4d4e2ccc2
Cconn92 <c.connors001@albright.edu>
2025-09-18 23:05:04 -0400

    Update asighnment4 work.py

--- asighnment4 work.py
+++ asighnment4 work.py
@@ -34,7 +34,7 @@ for number in range(1, 11):
 cubes = [number ** 3 for number in range(1, 11)]
 print(cubes)
 # 4.10
-pizzas = ['Pepperoni', 'Margarita', 'BBQ Chicken', 'Hawaiian', 'Veggie']
+pizzas = ['pepperoni', 'cheese', 'deluxe', 'Hawaian', 'suasage']
 print("The first three items in the list are:")
 print(pizzas[:3])
 middle_index = len(pizzas) // 2  
@@ -43,10 +43,10 @@ print(pizzas[middle_index-1:middle_index+2])
 print("\nThe last three items in the list are:")
 print(pizzas[-3:])
 #4.11
-pizzas = ['Pepperoni', 'Margarita', 'BBQ Chicken']
+pizzas = ['pepperoni', 'cheese', 'deluxe']
 friend_pizzas = pizzas[:]
-pizzas.append('Hawaiian')
-friend_pizzas.append('Veggie')
+pizzas.append('hawaian')
+friend_pizzas.append('suasage')
 print("My favorite pizzas are:")
 for pizza in pizzas:
     print(pizza)
@@ -54,16 +54,16 @@ for pizza in pizzas:
 print("\nMy friend's favorite pizzas are:")
 for pizza in friend_pizzas:
     print(pizza)
-foods = ['Pizza', 'Sushi', 'Burgers', 'Pasta']
+foods = ['pizza', 'sushi', 'burgers', 'pasta']
 print("My favorite foods are:")
 for food in foods:
     print(food)
-friend_foods = ['Tacos', 'Salad', 'Pasta', 'Steak']
+friend_foods = ['taco', 'salad', 'pasta', 'steak']
 print("\nMy friend's favorite foods are:")
 for food in friend_foods:
     print(food)
 # 4.13
-buffet_menu = ('Pizza', 'Sushi', 'Pasta', 'Burger', 'Salad')
+buffet_menu = ('pizza', 'sushi', 'pasta', 'burger', 'salad')
 print("The restaurant offers the following foods:")
 for food in buffet_menu:
     print(food)
@@ -71,15 +71,16 @@ try:
     buffet_menu[1] = 'Tacos' 
 except TypeError:
     print("\nYou can't modify a tuple!")
-buffet_menu = ('Pizza', 'Tacos', 'Pasta', 'Steak', 'Fruit Salad')
+buffet_menu = ('pizza', 'taco', 'pasta', 'steak', 'cake')
 print("\nThe updated menu includes the following foods:")
 for food in buffet_menu:
     print(food)
 #4.14
     def mypizzas():
         print("My Favorite Pizzas Are:")
-pizzas = ['Pepperoni','Margarita','BBQChicken','Veggie']
+pizzas = ['pepperoni','cheese','deluxe','suasage']
 for p in pizzas: print(p)
 
 mypizzas()
+
 
